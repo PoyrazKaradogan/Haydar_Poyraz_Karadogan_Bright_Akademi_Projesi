4d4d5b5747571f42c548e70db8b9519c32feb143
PoyrazKaradogan <hfk.1998@hotmail.com>
2024-01-16 18:44:20 +0300

    son

--- my-app/src/Pages/Contact/Contact.js
+++ my-app/src/Pages/Contact/Contact.js
@@ -19,13 +19,13 @@ const Contact = () => {
     return (
         <>
             <Container fluid className='d-flex flex-row page '>
-                <Row className='text-center ms-5'>    
+                <Row className='text-center '>    
                     
                     <Col xl="6">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3009.1384843889086!2d29.004241476042324!3d41.04410057134562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab7a24975fe5d%3A0xa05d7aa13cfcaf89!2sBah%C3%A7e%C5%9Fehir%20%C3%9Cniversitesi%20Wissen%20Akademie!5e0!3m2!1str!2str!4v1703543059614!5m2!1str!2str" width="500" height="500" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='my-5 '></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3009.1384843889086!2d29.004241476042324!3d41.04410057134562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab7a24975fe5d%3A0xa05d7aa13cfcaf89!2sBah%C3%A7e%C5%9Fehir%20%C3%9Cniversitesi%20Wissen%20Akademie!5e0!3m2!1str!2str!4v1703543059614!5m2!1str!2str" width="400" height="470" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='my-5 '></iframe>
                     </Col>
                     <Col xl="6">
-                        <Card className='p-3 my-5 me-2'>
+                        <Card className=' my-5 me-2'>
                             <Form noValidate validated ={context.val} onSubmit={context.handleSubmit}>
                                 <Row className="mb-4">
                                     <Form.Group as={Col} md="6" controlId="validationCustom01">
